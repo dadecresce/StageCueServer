5212a996e1de8f66d7ab448c4ad65b76a1eae559
dadecresce <dadecresce@gmail.com>
2025-04-27 10:25:02 +0200

    Restore routes() to satisfy TestHealthz and mount SFU/metrics

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,81 +1,91 @@
+// cmd/server/main.go
 package main
 
 import (
-    "context"
-    "flag"
-    "fmt"
-    "net/http"
-    "os"
-    "os/signal"
-    "syscall"
-    "time"
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-    "github.com/StageCue/StageCueServer/internal/config"
-    "github.com/StageCue/StageCueServer/internal/logger"
-    "go.uber.org/zap"
+	"github.com/StageCue/StageCueServer/internal/config"
+	"github.com/StageCue/StageCueServer/internal/logger"
+	"github.com/StageCue/StageCueServer/internal/metrics"
+	"github.com/StageCue/StageCueServer/internal/sfu"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 )
 
-var (
-    configPath string
-)
-
-func init() {
-    flag.StringVar(&configPath, "config", "config.toml", "path to configuration file")
-}
+var version = "dev"
 
 func main() {
-    flag.Parse()
+	var (
+		configPath string
+	)
+	flag.StringVar(&configPath, "config", "config.toml", "config file path")
+	flag.Parse()
 
-    cfg, err := config.Parse(configPath)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "failed to parse config: %v\n", err)
-        os.Exit(1)
-    }
+	cfg, err := config.Parse(configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to parse config: %v\n", err)
+		os.Exit(1)
+	}
 
-    log, err := logger.New(cfg.LogLevel)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "failed to init logger: %v\n", err)
-        os.Exit(1)
-    }
-    log.Info("StageCueServer starting",
-        zap.String("addr", cfg.Address),
-        zap.String("config", configPath),
-        zap.String("version", "v0.1"))
+	log, err := logger.New(cfg.LogLevel)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to init logger: %v\n", err)
+		os.Exit(1)
+	}
+	log.Info("StageCueServer starting",
+		zap.String("addr", cfg.Address),
+		zap.String("config", configPath),
+		zap.String("version", version))
 
-    srv := &http.Server{
-        Addr:         cfg.Address,
-        ReadTimeout:  5 * time.Second,
-        WriteTimeout: 10 * time.Second,
-        Handler:      routes(log),
-    }
+	// ---- SFU ---------------------------------------------------------------
+	sfuInstance, err := sfu.New(cfg, log)
+	if err != nil {
+		log.Fatal("failed to init SFU", zap.Error(err))
+	}
+	// -----------------------------------------------------------------------
 
-    go func() {
-        if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-            log.Fatal("listen error", zap.Error(err))
-        }
-    }()
-    log.Info("listening...", zap.String("addr", cfg.Address))
+	metrics.MustRegisterDefault() // Prom metrics
 
-    // graceful shutdown
-    sig := make(chan os.Signal, 1)
-    signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-    <-sig
-    log.Info("shutting down...")
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
-    if err := srv.Shutdown(ctx); err != nil {
-        log.Error("shutdown error", zap.Error(err))
-    }
-    log.Sync()
-}
+	mux := http.NewServeMux()
+	mux.Handle("/ws", sfuInstance.WebSocketHandler()) // signaling
+	mux.Handle("/metrics", promhttp.Handler())        // Prometheus
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprint(w, "ok")
+	})
+	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintf(w, "StageCueServer %s", version)
+	})
 
-func routes(log *zap.Logger) http.Handler {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "ok")
-    })
-    mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "StageCueServer v0.1")
-    })
-    return mux
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      mux,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("listen error", zap.Error(err))
+		}
+	}()
+	log.Info("listening...", zap.String("addr", cfg.Address))
+
+	// graceful shutdown
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
+	log.Info("shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("shutdown error", zap.Error(err))
+	}
+	log.Sync()
 }
